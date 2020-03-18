f30bac4aef999e2e30c1f514a1314f38bd05e6ef
Jamie Smith <i@jamiesmiths.com>
2020-03-18 00:55:46 -0400

    mock data for test

---
+++ test/test_sfdx_soql.js
@@ -0,0 +1,67 @@
+module.exports = {
+  "status": 0,
+  "result": {
+    "totalSize": 2,
+    "done": true,
+    "records": [
+      {
+        "attributes": {
+          "type": "Contact",
+          "url": "/services/data/v48.0/sobjects/Contact/0031U00000JajaoQAB"
+        },
+        "Id": "0031U00000JajaoQAB",
+        "Name": "Rose Gonzalez",
+        "Account": {
+          "attributes": {
+            "type": "Account",
+            "url": "/services/data/v48.0/sobjects/Account/0011U00000MV7G6QAL"
+          },
+          "Name": "Edge Communications",
+          "CreatedBy": {
+            "attributes": {
+              "type": "User",
+              "url": "/services/data/v48.0/sobjects/User/0051U000005O0HBQA0"
+            },
+            "Name": "Jamie Smith",
+            "CreatedBy": {
+              "attributes": {
+                "type": "User",
+                "url": "/services/data/v48.0/sobjects/User/0051U000005O0HBQA0"
+              },
+              "Name": "Jamie Smith"
+            }
+          }
+        }
+      },
+      {
+        "attributes": {
+          "type": "Contact",
+          "url": "/services/data/v48.0/sobjects/Contact/0031U00000JajapQAB"
+        },
+        "Id": "0031U00000JajapQAB",
+        "Name": "Sean Forbes",
+        "Account": {
+          "attributes": {
+            "type": "Account",
+            "url": "/services/data/v48.0/sobjects/Account/0011U00000MV7G6QAL"
+          },
+          "Name": "Edge Communications",
+          "CreatedBy": {
+            "attributes": {
+              "type": "User",
+              "url": "/services/data/v48.0/sobjects/User/0051U000005O0HBQA0"
+            },
+            "Name": "Jamie Smith",
+            "CreatedBy": {
+              "attributes": {
+                "type": "User",
+                "url": "/services/data/v48.0/sobjects/User/0051U000005O0HBQA0"
+              },
+              "Name": "Jamie Smith"
+            }
+          }
+        }
+      }
+    ]
+  }
+}
